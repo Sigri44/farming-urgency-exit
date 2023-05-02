65c48ea27fd0287472cf1a5d7ccb1ddc42f311d4
Sylvain <sylvain.grisel@mtpelerin.com>
2023-05-02 23:58:09 +0200

    Update Velo

--- js/velodrome.js
+++ js/velodrome.js
@@ -62,6 +62,78 @@ const OPTIMISM_VELODROME_TOKENS = {
     },
 }
 
+const OPTIMISM_VELODROME_GAUGE = {
+    'sAMM-LUSD/DAI': {
+        'gaugeAddress': '0x8b1ad293f4C1D4f66643D779637C72EAEfbb37DA',
+        'pairAddress': '0xf12ba676f9045bd76f8280bf249cbe8d209276e2',
+        'isStable': true,
+        'tokensPair': {
+            'tokenA': '0xc40F949F8a4e094D1b49a23ea9241D289B7b2819', // LUSD
+            'tokenB': '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1' // DAI
+        }
+    },
+    'sAMM-USDC/LUSD' : {
+        'gaugeAddress': '0x631dCe3a422e1af1AD9d3952B06f9320e2f2ed72',
+        'pairAddress': '0x207addb05c548f262219f6bfc6e11c02d0f7fdbe',
+        'isStable': true,
+        'tokensPair': {
+            'tokenA': '0x7F5c764cBc14f9669B88837ca1490cCa17c31607', // USDC
+            'tokenB': '0xc40F949F8a4e094D1b49a23ea9241D289B7b2819' // LUSD
+        }
+    },
+    'vAMM-wstETH/WETH': {
+        'gaugeAddress': '0x150dc0e12d473347becd0f7352e9dae6cd30d8ab',
+        'pairAddress': '0xc6C1E8399C1c33a3f1959f2f77349D74a373345c',
+        'isStable': false,
+        'tokensPair': {
+            'tokenA': '0x1F32b1c2345538c0c6f582fCB022739c4A194Ebb', // wstETH
+            'tokenB': '0x4200000000000000000000000000000000000006' // WETH
+        }
+    },
+    'vAMM-WETH/rETH': {
+        'gaugeAddress': '0x89c1a33011fab92e497963a6fa069aee5c1f5d44',
+        'pairAddress': '0x985612ff2c9409174fedcff23d4f4761af124f88',
+        'isStable': false,
+        'tokensPair': {
+            'tokenA': '0x4200000000000000000000000000000000000006', // WETH
+            'tokenB': '0x9Bcef72be871e61ED4fBbc7630889beE758eb81D' // rETH
+        }
+    },
+}
+
+const ALLOWANCE_INFINITE = '115792089237316195423570985008687907853269984665640564039457584007913129639935'
+
+// Generate gauge contract
+async function generateLpContract(type, contractName) {
+    return new ethers.Contract(
+        OPTIMISM_VELODROME_GAUGE[contractName][type],
+        ABIS['OPTIMISM']['VELODROME']['VELODROME_GAUGE'],
+        PROVIDER
+    )
+}
+
+// Hydrate gauge LP
+async function hydrateGaugeTokens() {
+    const selectGauge = document.querySelector('#gauge-optimism-velodrome-select')
+    const selectGaugeLiquidity = document.querySelector('#gauge-liquidity-optimism-velodrome-select')
+
+    for (const token in OPTIMISM_VELODROME_GAUGE) {
+        const option = document.createElement('option')
+        option.value = token
+        option.text = token
+        selectGauge.appendChild(option)
+        // selectGaugeLiquidity.appendChild(option)
+    }
+
+    for (const token in OPTIMISM_VELODROME_GAUGE) {
+        const option = document.createElement('option')
+        option.value = token
+        option.text = token
+        // selectGauge.appendChild(option)
+        selectGaugeLiquidity.appendChild(option)
+    }
+}
+
 // Hydrate liquidity tokens
 async function hydrateLiquidityTokens() {
     const selectTokenA = document.querySelector('#tokena-optimism-velodrome-select')
@@ -72,6 +144,14 @@ async function hydrateLiquidityTokens() {
         option.value = token
         option.text = token
         selectTokenA.appendChild(option)
+        // selectTokenB.appendChild(option)
+    }
+    
+    for (const token in OPTIMISM_VELODROME_TOKENS) {
+        const option = document.createElement('option')
+        option.value = token
+        option.text = token
+        // selectTokenA.appendChild(option)
         selectTokenB.appendChild(option)
     }
 }
@@ -80,95 +160,71 @@ $(document).ready(async function() {
     console.log("DEBUG::Velodrome loaded.")
 
     // Init vTokensList
-    await hydrateLiquidityTokens()
-
-    // PerpV2 - Optimism - Close Position
-    async function closeOptimismPerpPosition(button) {
-        const row = button.parent().parent()
-        console.log("row::", row)
-        const vToken = row[0].cells[0].textContent
-        const sidePosition = row[0].cells[1].textContent
-        const amount = Number(row[0].cells[2].textContent)
-        const slippage = Number(row[0].cells[3].getElementsByTagName('input')[0].value)
-        const currentDate = new Date()
-        const baseToken = PERPV2_METADATA['contracts'][vToken]['address']
-        let oppositeAmountBound = 0
-        const sqrtPriceLimitX96 = "0"
-        const timestamp = currentDate.getTime();
-        const deadline = timestamp + 300000; // + 5 minutes
-        const referralCode = "0x0000000000000000000000000000000000000000000000000000000000000000"
-
-        const chainlinkOraclePriceFeed = await getChainlinkOraclePriceFeed(PERPV2_METADATA['contracts'][vToken + 'vUSDUniswapV3PriceFeed']['address'])
-        const price = Number(chainlinkOraclePriceFeed)
-        console.log("price::", typeof price, price)
-        console.log("amount::", typeof amount, amount)
-        console.log("slippage::", typeof slippage, slippage)
-
-        if (sidePosition == 'long') {
-            oppositeAmountBound = (price - ((price * slippage) / 100)) * amount
-        } else {
-            oppositeAmountBound = (price + ((price * slippage) / 100)) * amount
-        }
-        console.log("oppositeAmountBound::", typeof oppositeAmountBound, oppositeAmountBound)
-
-        // DEBUG transaction
-        console.log("transaction_debug::", {
-            "baseToken": baseToken,
-            "sqrtPriceLimitX96": sqrtPriceLimitX96,
-            "oppositeAmountBound": oppositeAmountBound,
-            "deadline": deadline,
-            "referralCode": referralCode
-        })
-
-        const clearingHouseContract = await OPTIMISM_PERP_CLEARINGHOUSE_PROXY_INTERACTION.connect(SIGNER)
-
-        const tx = await clearingHouseContract.closePosition([
-            baseToken,
-            sqrtPriceLimitX96,
-            ethers.utils.parseUnits(oppositeAmountBound.toString(), 18),
-            deadline,
-            referralCode,
-            {
-                gasLimit: 1500000
-            }
-
-            // address baseToken; -> vToken base address
-            // uint160 sqrtPriceLimitX96; -> is a restriction on the ending price after the swap, where sqrtPriceLimitX96 is defined here. For no restriction, you can enter 0. ???
-            // uint256 oppositeAmountBound; -> is a restriction on how many tokens to receive or pay, depending on isBaseToQuote and isExactInput. ??? (vUSD VALUE !!???)
-            // uint256 deadline; -> sets a deadline for executing the transaction. You can use a Unix time converter to enter this parameter, by converting a custom date 15 minutes ahead of the current time to Unix timestamp: https://awebanalysis.com/en/unix-timestamp-converter/ ???
-            // bytes32 referralCode; -> is used to add your referral code (or use 0x0000000000000000000000000000000000000000000000000000000000000000 if you do not have a code to apply). ???
-        ])
-
-        console.log("tx::", tx)
-    }
-    // PerpV2 - Optimism - Close position on click
-    $('#perp-positions').on('click', '.close-prep-optimism-position-button', function() {
-        const button = $(this)
-        closeOptimismPerpPosition(button)
-    })
+    // await hydrateLiquidityTokens()
+    await hydrateGaugeTokens()
 
     // deposit (Gauge1) : https://optimistic.etherscan.io/tx/0x858cc79b2d6718be07648f972ec6f13a007f3ec0c73e5723fad4b8ca7fd5ad51
     // withdraw (Gauge2) : https://optimistic.etherscan.io/tx/0xd12a48e2955b9cd5f639f11dd274058e057c28d55ca3856ef13cabe192e7d9c8
     // addLiquidity (router) : https://optimistic.etherscan.io/tx/0x25b18e5022bd2ad6d0cdc3054b4dc01be6cf6b1f01a3d2d10478e97245f11e57
     // removeLiquidity (router) : https://optimistic.etherscan.io/tx/0x845f74d58cf44e99367de28d8059a3b6a4c8b6bfb83ff769a6c012d261ea0f04
 
+    // Withdraw (Unstake)
+    $("#withdraw-optimism-velodrome-button").click(async function() {
+        const contractName = $("#gauge-optimism-velodrome-select").val()
+        const OPTIMISM_VELODROME_GAUGE_INTERACTION = await generateLpContract('gaugeAddress', contractName)
+        console.log('OPTIMISM_VELODROME_GAUGE_INTERACTION::', typeof OPTIMISM_VELODROME_GAUGE_INTERACTION, OPTIMISM_VELODROME_GAUGE_INTERACTION)
+        const withdrawLP = await OPTIMISM_VELODROME_GAUGE_INTERACTION.withdrawAll()
+
+        console.log("withdrawLP::", withdrawLP)
+    })
+
+    // Allowance remove liquidity
+    $("#allowance-remove-liquidity-optimism-velodrome-button").click(async function() {
+        const contractName = $("#gauge-liquidity-optimism-velodrome-select").val()
+        const contractGaugeLP = generateLpContract('pairAddress', contractName)
+        const allowanceLP = await contractGaugeLP.approve({
+            OPTIMISM_VELODROME_ROUTER_CONTRACT,
+            ALLOWANCE_INFINITE
+        })
+
+        console.log("allowanceLP::", allowanceLP)
+    })
+
     // Velodrome - Optimism - 
-    $("#add-liquidity-optimism-velodrome-button").click(async function() {
-        // DEBUG : Price
+    $("#remove-liquidity-optimism-velodrome-button").click(async function() {
+        // DEBUG : FAKE Price
         const fakePrice = 0.70
+        const price = fakePrice
 
         const currentDate = new Date()
         const timestamp = currentDate.getTime()
         const deadline = timestamp + 300000 // + 5 minutes
-        const tokenA = $("tokena-optimism-velodrome-select").val()
-        const tokenB = $("tokenb-optimism-velodrome-select").val()
-        const liquidity = $("withdraw-optimism-velodrome-vault-amount").val() // amount of LP tokens
-        const isStable = $("type-optimism-velodrome-select").val()
-        const amountAMin = (uint256) // slippage
-        const amountBMin = (uint256) // slippage
+        const gauge = $("gauge-liquidity-optimism-velodrome-select").val()
+        const tokenA = OPTIMISM_VELODROME_GAUGE[gauge]['tokensPair']['tokenA']
+        const tokenB = OPTIMISM_VELODROME_GAUGE[gauge]['tokensPair']['tokenB']
+        const fakeLiquidity = 0.128
+        // const liquidity = $("withdraw-optimism-velodromevelodrome-vault-amount").val() // amount of LP tokens
+        const liquidity = fakeLiquidity
+        // const slippage = Number(row[0].cells[3].getElementsByTagName('input')[0].value)
+        const slippage = Number(1)
+        const isStable = OPTIMISM_VELODROME_GAUGE[gauge]['isStable']
+        const amountAMin = (price - ((price * slippage) / 100)) * amount // slippage
+        const amountBMin = (price - ((price * slippage) / 100)) * amount // slippage
         const to = $("#selected-account").text()
+        
+         // DEBUG transaction
+         console.log("transaction_debug::", {
+            "tokenA": tokenA,
+            "tokenB": tokenB,
+            "liquidity": liquidity,
+            "isStable": isStable,
+            "amountAMin": amountAMin,
+            "amountBMin": amountBMin,
+            "to": to,
+            "deadline": deadline
+        })
 
-        const removeLiquidity = OPTIMISM_VELODROME_ROUTER_INTERACTION.removeLiquidity({
+        const removeLiquidity = await OPTIMISM_VELODROME_ROUTER_INTERACTION.removeLiquidity({
             tokenA,
             tokenB,
             liquidity,
@@ -179,8 +235,9 @@ $(document).ready(async function() {
             deadline,
         })
 
-
-
         console.log("removeLiquidity::", removeLiquidity)
     })
+
+    // Swap
+
 })
