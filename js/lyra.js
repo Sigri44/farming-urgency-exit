d3fe9fbdb6dd6146d558730c207720a941198abe
Sylvain <sylvain.grisel@mtpelerin.com>
2023-05-12 11:25:11 +0200

    Fix close Lyra arbi position

--- js/lyra.js
+++ js/lyra.js
@@ -1,5 +1,5 @@
 // Lyra contracts
-// ETH market (Newport) - Option Market
+// ETH market (Newport) - Option Market // ETH
 const ARBITRUM_LYRA_OPTIONMARKET_CONTRACT = '0x919E5e0C096002cb8a21397D724C4e3EbE77bC15'
 const ARBITRUM_LYRA_OPTIONMARKET_INTERACTION = new ethers.Contract(
     ARBITRUM_LYRA_OPTIONMARKET_CONTRACT,
@@ -15,7 +15,7 @@ const ARBITRUM_LYRA_OPTIONTOKEN_INTERACTION = new ethers.Contract(
     PROVIDER
 )
 
-// Shared Protocol addresses - Option Market
+// Shared Protocol addresses - Option Market // ETH
 const OPTIMISM_LYRA_OPTIONMARKET_CONTRACT = '0xCCE7819d65f348c64B7Beb205BA367b3fE33763B'
 const OPTIMISM_LYRA_OPTIONMARKET_INTERACTION = new ethers.Contract(
     OPTIMISM_LYRA_OPTIONMARKET_CONTRACT,
@@ -31,71 +31,92 @@ const OPTIMISM_LYRA_OPTIONTOKEN_INTERACTION = new ethers.Contract(
     PROVIDER
 )
 
-// Hydrate balances
+const OPTION_TYPE = [
+    "LONG_CALL",
+    "LONG_PUT",
+    "SHORT_CALL_BASE",
+    "SHORT_CALL_QUOTE",
+    "SHORT_PUT_QUOTE"
+]
+
+// Hydrate Arbitrum balances
 async function hydrateArbitrumPositions() {
     console.log("DEBUG:: Lyra Arbitrum positions hydrated - In progress")
     // Clean table
     document.querySelector("#lyra-arbitrum-positions tbody").innerHTML = ""
 
     const listPositions = await ARBITRUM_LYRA_OPTIONTOKEN_INTERACTION.getOwnerPositions(
-        // $("#selected-account").text()
-        "0x233f681ff98e18fa46fd9d6203520a6a301aeaa1"
+        $("#selected-account").text()
     )
 
     console.log("listPositions::", typeof listPositions, listPositions)
 
     for (const id in listPositions) {
-        // const contractGauge = OPTIMISM_VELODROME_GAUGE[pair]['gaugeAddress']
-        // const contractPair = OPTIMISM_VELODROME_GAUGE[pair]['pairAddress']
-        // const stakedBalance = await getErc20Balance(contractGauge, 18)
-        // const unstakedBalance =  await getErc20Balance(contractPair, 18)
-        const col0 = listPositions[id][0]
-        const col1 = listPositions[id][1]
-        const col2 = listPositions[id][2]
-        const col3 = listPositions[id][3]
-        const col4 = listPositions[id][4]
-        const col5 = listPositions[id][5]
+        const positionId = listPositions[id][0]
+        const strikeId = listPositions[id][1]
+        const optionType = listPositions[id][2]
+        const amount = ethers.utils.formatEther(listPositions[id][3])
+        const collateral = Number(ethers.utils.formatEther(listPositions[id][4])).toFixed(6)
+        // const state = listPositions[id][5]
 
-        addArbitrumPositionRow(col0, col1, col2, col3, col4, col5)
+        addArbitrumPositionRow(positionId, strikeId, optionType, amount, collateral)
     }
     console.log("DEBUG:: Lyra Arbitrum positions hydrated - Finished !")
 }
 
-function addArbitrumPositionRow(col0, col1, col2, col3, col4, col5) {
-    // col0
+function addArbitrumPositionRow(positionId, strikeId, optionType, amount, collateral) {
+    // positionId
     var x = document.createElement("tr")
     var a = document.createElement("td")
-    var anode = document.createTextNode(col0)
+    var anode = document.createTextNode(positionId)
     a.appendChild(anode)
     x.appendChild(a)
 
-    // col1
+    // strikeId
     a = document.createElement("td")
-    anode = document.createTextNode(col1)
+    anode = document.createTextNode(strikeId)
     a.appendChild(anode)
     x.appendChild(a)
 
-    // col2
+    // optionType
     a = document.createElement("td")
-    anode = document.createTextNode(col2)
+    anode = document.createTextNode(OPTION_TYPE[optionType])
     a.appendChild(anode)
     x.appendChild(a)
 
-    // col3
+    // amount
     a = document.createElement("td")
-    anode = document.createTextNode(col3)
+    anode = document.createTextNode(amount)
     a.appendChild(anode)
     x.appendChild(a)
 
-    // col4
+    // collateral
     a = document.createElement("td")
-    anode = document.createTextNode(col4)
+    anode = document.createTextNode(collateral)
     a.appendChild(anode)
     x.appendChild(a)
 
-    // col5
+    // minTotalCost
     a = document.createElement("td")
-    anode = document.createTextNode(col5)
+    anode = document.createElement('input')
+    anode.setAttribute('type', 'number')
+    anode.setAttribute('value', '0')
+    a.appendChild(anode)
+    x.appendChild(a)
+
+    //maxTotalCost
+    a = document.createElement("td")
+    anode = document.createElement('input')
+    anode.setAttribute('type', 'number')
+    anode.setAttribute('value', '0')
+    a.appendChild(anode)
+    x.appendChild(a)
+
+    //inputAmount
+    a = document.createElement("td")
+    anode = document.createElement('input')
+    anode.setAttribute('type', 'number')
+    anode.setAttribute('value', '0')
     a.appendChild(anode)
     x.appendChild(a)
     
@@ -111,35 +132,129 @@ function addArbitrumPositionRow(col0, col1, col2, col3, col4, col5) {
     document.querySelector("#lyra-arbitrum-positions tbody").appendChild(x)
 }
 
+// Hydrate Optimism balances
+async function hydrateOptimismPositions() {
+    console.log("DEBUG:: Lyra Optimism positions hydrated - In progress")
+    // Clean table
+    document.querySelector("#lyra-optimism-positions tbody").innerHTML = ""
+
+    const listPositions = await OPTIMISM_LYRA_OPTIONTOKEN_INTERACTION.getOwnerPositions(
+        $("#selected-account").text()
+    )
+
+    console.log("listPositions::", typeof listPositions, listPositions)
+
+    for (const id in listPositions) {
+        const positionId = listPositions[id][0]
+        const strikeId = listPositions[id][1]
+        const optionType = listPositions[id][2]
+        const amount = ethers.utils.formatEther(listPositions[id][3])
+        const collateral = Number(ethers.utils.formatEther(listPositions[id][4])).toFixed(6)
+        // const state = listPositions[id][5]
+
+        addOptimismPositionRow(positionId, strikeId, optionType, amount, collateral)
+    }
+    console.log("DEBUG:: Lyra Optimism positions hydrated - Finished !")
+}
+
+function addOptimismPositionRow(positionId, strikeId, optionType, amount, collateral) {
+    // positionId
+    var x = document.createElement("tr")
+    var a = document.createElement("td")
+    var anode = document.createTextNode(positionId)
+    a.appendChild(anode)
+    x.appendChild(a)
+
+    // strikeId
+    a = document.createElement("td")
+    anode = document.createTextNode(strikeId)
+    a.appendChild(anode)
+    x.appendChild(a)
+
+    // optionType
+    a = document.createElement("td")
+    anode = document.createTextNode(OPTION_TYPE[optionType])
+    a.appendChild(anode)
+    x.appendChild(a)
+
+    // amount
+    a = document.createElement("td")
+    anode = document.createTextNode(amount)
+    a.appendChild(anode)
+    x.appendChild(a)
+
+    // collateral
+    a = document.createElement("td")
+    anode = document.createTextNode(collateral)
+    a.appendChild(anode)
+    x.appendChild(a)
+
+    // minTotalCost
+    a = document.createElement("td")
+    anode = document.createElement('input')
+    anode.setAttribute('type', 'number')
+    anode.setAttribute('value', '0')
+    a.appendChild(anode)
+    x.appendChild(a)
+
+    //maxTotalCost
+    a = document.createElement("td")
+    anode = document.createElement('input')
+    anode.setAttribute('type', 'number')
+    anode.setAttribute('value', '0')
+    a.appendChild(anode)
+    x.appendChild(a)
+
+    //inputAmount
+    a = document.createElement("td")
+    anode = document.createElement('input')
+    anode.setAttribute('type', 'number')
+    anode.setAttribute('value', '0')
+    a.appendChild(anode)
+    x.appendChild(a)
+    
+    // Button
+    a = document.createElement("td")
+    anode = document.createElement('button')
+    anode.setAttribute('type', 'button')
+    anode.setAttribute('class', 'btn btn-dark close-position-optimism-lyra-button')
+    anode.innerText = 'Close position'
+    a.appendChild(anode)
+    x.appendChild(a)
+
+    document.querySelector("#lyra-optimism-positions tbody").appendChild(x)
+}
+
 $(document).ready(async function() {
     console.log("DEBUG::Lyra loaded.")
     /***********
     * ARBITRUM *
     ************/
-    // Lyra - Arbitrum - Check balances
-    $("#check-balances-arbitrum-lyra-button").click(async function() {
-        // balanceOfWei = await OPTIMISM_PERP_VAULT_INTERACTION.getBalanceByToken(
-        // tuppleResponse = await ARBITRUM_LYRA_OPTIONMARKET_INTERACTION.getOwnerPositions(
-        //     $("#selected-account").text()
-        // )
-
-        // console.log("tuppleResponse::", typeof tuppleResponse, tuppleResponse)
-
-        // balanceOf = ethers.utils.formatUnits(balanceOfWei, 18)
-        // $("#usdc-optimism-perp-balance").text(balanceOf)
+    // Lyra - Arbitrum - Check positions
+    $("#check-positions-arbitrum-lyra-button").click(async function() {
+        await hydrateArbitrumPositions()
     })
-    // Lyra - Arbitrum - Sell ETH CALL
-    $("#close-position-arbitrum-lyra-optionmarket-button").click(async function() {
-        const currentDate = new Date();
-        const vtoken = $("#open-position-vtoken-optimism-perp-select").val()
-        const amount = $("#open-position-optimism-perp-clearinghouse-amount").val()
-        const isBaseToQuote = JSON.parse($("#open-position-side-optimism-perp-select").val())
-        const isExactInput = true
-        const oppositeAmountBound = "0"
-        const sqrtPriceLimitX96 = "0"
-        const timestamp = currentDate.getTime();
-        const deadline = timestamp + 300000; // + 5 minutes
-        const referralCode = "0x0000000000000000000000000000000000000000000000000000000000000000"
+
+    // Lyra - Arbitrum - Close position
+    async function closePositionArbitrumLyraLp(button) {
+        const row = button.parent().parent()
+        console.log("row::", row)
+        const strikeId = row[0].cells[1].textContent
+        const positionId = row[0].cells[0].textContent
+        const iterations = 1 // no need to optimize iterations as amount does not change
+        const optionType =  OPTION_TYPE.indexOf(row[0].cells[2].textContent)
+        const amount = Number(row[0].cells[3].textContent)
+        const setCollateralTo = "0"
+        const minTotalCost = row[0].cells[5].getElementsByTagName('input')[0].value
+        let maxTotalCost = row[0].cells[6].getElementsByTagName('input')[0].value
+        const referrer = "0x0000000000000000000000000000000000000000"
+
+        // Set max int value if maxTotalCost is not define
+        if (maxTotalCost == 0) {
+            maxTotalCost = MAX_INT
+        } else {
+            maxTotalCost = ethers.utils.parseEther(maxTotalCost)
+        }
 
         // Debug transaction
         const transaction_debug = {
@@ -151,7 +266,7 @@ $(document).ready(async function() {
             "setCollateralTo": setCollateralTo,
             "minTotalCost": minTotalCost,
             "maxTotalCost": maxTotalCost,
-            "referralCode": referralCode
+            "referrer": referrer
         }
         console.log("transaction_debug::", transaction_debug)
 
@@ -162,43 +277,14 @@ $(document).ready(async function() {
             positionId,
             iterations,
             optionType,
-            amount,
-            setCollateralTo,
-            minTotalCost,
+            ethers.utils.parseEther(amount.toString()),
+            ethers.utils.parseEther(setCollateralTo.toString()),
+            ethers.utils.parseEther(minTotalCost.toString()),
             maxTotalCost,
-            referralCode,
-
-            // 0 - params.strikeId	: uint256	231
-            // 1 - params.positionId	: uint256	7921
-            // 2 - params.iterations	: uint256	3
-            // 3 - params.optionType	: uint8	0
-            // 4 - params.amount	: uint256	1000000000000000000
-            // 5 - params.setCollateralTo	: uint256	0
-            // 6 - params.minTotalCost	: uint256	4644766700520098715
-            // 7 - params.maxTotalCost	: uint256	115792089237316195423570985008687907853269984665640564039457584007913129639935
-            // 8 - params.referrer	: address	0x0000000000000000000000000000000000000000
+            referrer
         ])
 
         console.log("tx::", tx)
-    })
-
-    // Lyra - Arbitrum - Check positions
-    $("#check-positions-arbitrum-lyra-button").click(async function() {
-        await hydrateArbitrumPositions()
-    })
-
-    // Lyra - Arbitrum - Close position
-    async function closePositionArbitrumLyraLp(button) {
-        const row = button.parent().parent()
-        console.log("row::", row)
-        const contractName = row[0].cells[0].textContent
-        const contractGaugeLP = await generateLpContract('gaugeAddress', contractName)
-        const OPTIMISM_VELODROME_GAUGE_INTERACTION = await contractGaugeLP.connect(SIGNER)
-
-        console.log('OPTIMISM_VELODROME_GAUGE_INTERACTION::', typeof OPTIMISM_VELODROME_GAUGE_INTERACTION, OPTIMISM_VELODROME_GAUGE_INTERACTION)
-        const withdrawLP = await OPTIMISM_VELODROME_GAUGE_INTERACTION.withdrawAll()
-
-        console.log("withdrawLP::", withdrawLP)
     }
     // Lyra - Arbitrum - Close position on click
     $('#lyra-arbitrum-positions').on('click', '.close-position-arbitrum-lyra-button', function() {
@@ -209,31 +295,27 @@ $(document).ready(async function() {
     /***********
     * OPTIMISM *
     ************/
-    // Lyra - Optimism - Check balances
-    $("#check-balances-optimism-lyra-button").click(async function() {
-        // balanceOfWei = await OPTIMISM_PERP_VAULT_INTERACTION.getBalanceByToken(
-        tuppleResponse = await ARBITRUM_LYRA_OPTIONMARKET_INTERACTION.getOwnerPositions(
-            $("#selected-account").text()
-        )
-
-        console.log("tuppleResponse::", typeof tuppleResponse, tuppleResponse)
-
-        // balanceOf = ethers.utils.formatUnits(balanceOfWei, 18)
-        // $("#usdc-optimism-perp-balance").text(balanceOf)
+    // Lyra - Optimism - Check positions
+    $("#check-positions-optimism-lyra-button").click(async function() {
+        console.log("DEBUG::button started")
+        await hydrateOptimismPositions()
     })
 
-    // Lyra - Optimism - Sell ETH CALL
-    $("#close-position-optimism-lyra-button").click(async function() {
-        const currentDate = new Date();
-        const vtoken = $("#open-position-vtoken-optimism-perp-select").val()
-        const amount = $("#open-position-optimism-perp-clearinghouse-amount").val()
-        const isBaseToQuote = JSON.parse($("#open-position-side-optimism-perp-select").val())
-        const isExactInput = true
-        const oppositeAmountBound = "0"
-        const sqrtPriceLimitX96 = "0"
-        const timestamp = currentDate.getTime();
-        const deadline = timestamp + 300000; // + 5 minutes
-        const referralCode = "0x0000000000000000000000000000000000000000000000000000000000000000"
+    // Lyra - Optimism - Close position
+    async function closePositionOptimismLyraLp(button) {
+        const row = button.parent().parent()
+        console.log("row::", row)
+        const optionMarket = OPTIMISM_LYRA_OPTIONMARKET_CONTRACT // ETH
+        const strikeId = row[0].cells[1].textContent
+        const positionId = row[0].cells[0].textContent
+        const iterations = 1 // no need to optimize iterations as amount does not change
+        const setCollateralTo = 0
+        const currentCollateral = row[0].cells[4].textContent
+        const amount = row[0].cells[3].textContent
+        const minTotalCost = Number(row[0].cells[5].getElementsByTagName('input')[0].value)
+        const maxTotalCost = Number(row[0].cells[6].getElementsByTagName('input')[0].value)
+        const inputAmount = Number(row[0].cells[7].getElementsByTagName('input')[0].value)
+        const inputAsset = "0x7F5c764cBc14f9669B88837ca1490cCa17c31607" // USDC
 
         // Debug transaction
         const transaction_debug = {
@@ -241,45 +323,37 @@ $(document).ready(async function() {
             "strikeId": strikeId,
             "positionId": positionId,
             "iterations": iterations,
-            "optionType": optionType,
-            "amount": amount,
             "setCollateralTo": setCollateralTo,
+            "currentCollateral": currentCollateral,
+            "amount": amount,
             "minTotalCost": minTotalCost,
             "maxTotalCost": maxTotalCost,
-            "referralCode": referralCode
+            "inputAmount": inputAmount,
+            "inputAsset": inputAsset
         }
         console.log("transaction_debug::", transaction_debug)
 
-        const optionMarketContract = await ARBITRUM_LYRA_OPTIONMARKET_INTERACTION.connect(SIGNER)
+        const optionMarketContract = await OPTIMISM_LYRA_OPTIONMARKET_INTERACTION.connect(SIGNER)
 
         const tx = await optionMarketContract.closePosition([
             optionMarket,
             strikeId,
             positionId,
             iterations,
-            optionType,
-            amount,
-            setCollateralTo,
-            minTotalCost,
-            maxTotalCost,
-            referralCode,
-
-            // 0 - params.optionMarket	: address	0x1d42a98848e022908069c2c545aE44Cc78509Bc8
-            // 1 - params.strikeId	: uint256	231
-            // 2 - params.positionId	: uint256	7921
-            // 3 - params.iterations	: uint256	3
-            // 4 - params.setCollateralTo	: uint256	0
-            // 5 - params.currentCollateral	: uint256	1407009431295292672
-            // 6 - params.amount	: uint256	1000000000000000000
-            // 7 - params.minTotalCost	: uint256	4644766700520098715
-            // 8 - params.maxTotalCost	: uint256	115792089237316195423570985008687907853269984665640564039457584007913129639935
-            // 9 - params.inputAmount	: uint256	379387823909808898048
-            // 10 - params.inputAsset	: address	0x0000000000000000000000000000000000000000
-
-            // Ex close 1 sETH contract :
-            // ["0xc7f1A22c30aE981E6A74a0267CE6cBBF27D8ecD5", "61", "0", "1", "0", "0", "1000000000000000", "0", "72640965621474238", "73509", "0x7F5c764cBc14f9669B88837ca1490cCa17c31607"]
+            ethers.utils.parseUnits(setCollateralTo.toString(), 18),
+            ethers.utils.parseUnits(currentCollateral.toString(), 18),
+            ethers.utils.parseUnits(amount.toString(), 18),
+            ethers.utils.parseUnits(minTotalCost.toString(), 18),
+            ethers.utils.parseUnits(maxTotalCost.toString(), 18),
+            ethers.utils.parseUnits(inputAmount.toString(), 18),
+            inputAsset
         ])
 
-        console.log("tx::", tx)
+         console.log("tx::", tx)
+    }
+    // Lyra - Optimism - Close position on click
+    $('#lyra-optimism-positions').on('click', '.close-position-optimism-lyra-button', function() {
+        const button = $(this)
+        closePositionOptimismLyraLp(button)
     })
 })
